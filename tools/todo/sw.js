2d68203bac52ba35c2c047f6168c595583e0c448
comame <dev@comame.xyz>
2020-04-03 20:20:13 +0900

    pwa

---
+++ tools/todo/sw.js
@@ -0,0 +1,16 @@
+const CACHE = {
+    key_prefix: 'todo',
+    key: '20200403-1',
+    files: [
+        './',
+        './app.js',
+        './database.js',
+        './icon-144.png',
+        './index.html',
+        './style.css',
+        './manifest.json',
+        'https://comame.xyz/assets/script/sw-template.js'
+    ]
+}
+
+self.importScripts("https://comame.xyz/assets/script/sw-template.js")
